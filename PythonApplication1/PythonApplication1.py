fff62e0c7c32f2670095387953cbc77da35b1025
aishanaz2984 <aishanaz2984@gmail.com>
2020-08-09 19:52:41 -0400

    Delete PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++
@@ -1,7 +0,0 @@
-from keras.datasets import mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-import matplotlib.pyplot as plt
-image_index = 7777 # You may select anything up to 60,000
-print(y_train[image_index]) # The label is 8
-plt.imshow(x_train[image_index], cmap='Greys')
-plt.show()
